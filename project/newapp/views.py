a6708ba39f18b64493e8bfb6e95eaecab1b3e567
Andr <amyagkov66@mail.ru>
2023-04-30 17:31:03 +0300

    D2_2

---
+++ project/newapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# from django.shortcuts import render
+# from django.views.generic import ListView  # импортируем класс, который говорит нам о том, что в этом представлении мы будем выводить список объектов из БД
+# from .models import Author
+#
+#
+# class AuthorList(ListView):
+#     model = Author  # указываем модель, объекты которой мы будем выводить
+#     template_name = 'products.html'  # указываем имя шаблона, в котором будет лежать HTML, в нём будут все инструкции о том, как именно пользователю должны вывестись наши объекты
+#     context_object_name = 'products'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
+#
